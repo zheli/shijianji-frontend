356f9a32bc2a5415da3cc4d3285fe3f3129e046b
Zhe Li <linuxcity.jn@gmail.com>
2020-08-17 00:08:53 +0200

    Add store with redux devtool support

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from './_helpers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
